5868af0857ed64069cd3bbc54f08c927a6ef0f15
Patryk Sypuła <UL0253548@edu.uni.lodz.pl>
2022-05-13 18:53:20 +0200

    Binding works finally

---
+++ Model-View-ViewModel/Model-View-ViewModel/Models/Words.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model_View_ViewModel.Models
+{
+    public class Words
+    {
+
+        public string FirstExampleString;
+
+        public string SecondExampleString;
+
+        public string ConnectedString;
+
+    }
+}
---
+++ Model-View-ViewModel/Model-View-ViewModel/ViewModels/WordsViewModel.cs
@@ -0,0 +1,64 @@
+﻿using Model_View_ViewModel.Models;
+using System;
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model_View_ViewModel.ViewModels
+{
+
+    public class WordsViewModel : INotifyPropertyChanged
+    {
+        private Words model = new Words();
+        public string FirstString
+        {
+            get { return model.FirstExampleString; }
+            set
+            {
+                if (model.FirstExampleString != value)
+                {
+                    model.FirstExampleString = value;
+                    OnPropertyChanged("FirstString");
+                    OnPropertyChanged("ResultString");
+                }
+            }
+        }
+        public string SecondString
+        {
+            get { return model.SecondExampleString; }
+            set
+            {
+                if (model.SecondExampleString != value)
+                {
+                    model.SecondExampleString = value;
+                    OnPropertyChanged("SecondString");
+                    OnPropertyChanged("ResultString");
+                }
+            }
+        }
+
+        public string ResultString
+        {
+            get { return model.ConnectedString = model.FirstExampleString + " " + model.SecondExampleString; }
+            set
+            {
+                model.ConnectedString = value;
+                OnPropertyChanged("ResultString");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
